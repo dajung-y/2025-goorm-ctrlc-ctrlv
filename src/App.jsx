8b23758b04ff1a71c26e7e5776f9c8597b3833f8
kimhana415 <khnaa415@gmail.com>
2025-06-18 15:42:36 +0900

    feat: Button component 추가 및 기본 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainPage from './pages/MainPage'
+import ProductListPage from './pages/ProductListPage'
+import ProductDetailPage from './pages/ProductDetailPage'
 
 export default function App() {
   return (
     <div>
-      App
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/list' element={<ProductListPage />} />
+        <Route path='/detail' element={<ProductDetailPage />} />
+      </Routes>
+
+      <Footer />
     </div>
   )
 }

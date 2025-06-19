749de8df3bdcda90c47eab4fd667df2c3d7276f3
lee02g29 <lee02g29@naver.com>
2025-06-20 02:28:26 +0900

    Feat : 상세페이지 공간 분리

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import MainPage from './pages/MainPage'
 import ProductListPage from './pages/ProductListPage'
-import ProductDetailPage from './pages/ProductDetailPage'
+import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage'
 
 export default function App() {
   return (
---
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./productDetailPage.css";
+
+export default function ProductDetailPage() {
+  return (
+    <main className="productDetail__container">
+      <section className="productDetail__content">
+        <article className="productDetail__content--desc">섹션</article>
+        <aside className="productDetail__content--side">사이드</aside>
+      </section>
+    </main>
+  );
+}

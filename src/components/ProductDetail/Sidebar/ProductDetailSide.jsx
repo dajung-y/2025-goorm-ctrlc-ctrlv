ef5630fac959529b5666847a6624858ac1e3a980
lee02g29 <lee02g29@naver.com>
2025-06-27 16:13:54 +0900

    Feat : 첫 구매시 쿠폰 공간 추가

--- src/components/ProductDetail/Sidebar/ProductDetailSide.jsx
+++ src/components/ProductDetail/Sidebar/ProductDetailSide.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductDetailSideCategory from "./ProductDetailSideCategory";
 import StarIcon from "../../../assets/StarIcon";
 import UsersIcon from "../../../assets/UsersIcon";
 
 export default function ProductDetailSide() {
+  const [price, setPrice] = useState(49900);
+  const [discountPrice, setDiscountPrice] = useState(price - 5000);
+
   return (
     // 사이드바
     <section className="productDetail__side--category">
@@ -24,14 +27,41 @@ export default function ProductDetailSide() {
 
       {/* 별점 & 참여자 */}
       <article className="productDetail__side--prticipant flex flex-row items-center mt-3">
+        {/* 별점 */}
         <div className="productDetail_-side--scores flex flex-row items-center mr-4 gap-2">
           <StarIcon size={20} />
           <span>
             {4.6} ({7})
           </span>
         </div>
+        {/* 참여자 수 */}
         <div className="productDetail_-side--scores flex flex-row items-center gap-2">
           <UsersIcon size={20} /> <span>{117}명 참여</span>
+        </div>
+      </article>
+
+      {/* 가격 */}
+      <article className="productDetail__side--priceInfo mb-6 mt-6">
+        <span className="text-2xl font-bold">
+          {price.toLocaleString("ko-KR")}원
+        </span>
+      </article>
+
+      {/* 첫 결제시 쿠폰 */}
+      <article className="productDetail__side--firstCoupon flex flex-row items-center justify-between p-4 border-1 border-gray-300 rounded-lg">
+        <div className="flex flex-col justify-center">
+          <span>
+            첫 결제 시 <strong className="text-[#F66F66]">{5000}원</strong> 즉시
+            적용
+          </span>
+          <span className="text-2xl font-bold">
+            {discountPrice.toLocaleString("ko-KR")}원
+          </span>
+        </div>
+
+        <div>
+          <img src="https://static.wadiz.kr/assets/store/images/first_purchase_coupon.png"
+          className="w-15"></img>
         </div>
       </article>
     </section>

48d511716bb8be5cf75536e08b02cafdf49d91e3
dajung-y <0352wjd@naver.com>
2025-06-27 15:14:09 +0900

    feat: 다정: 배너 추가

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EarlyBirdProduct from './EarlyBirdProduct'
+import CreateProjectBanner from './CreateProjectBanner'
+
+export default function MainContainer() {
+  return (
+    <main>
+      <EarlyBirdProduct />
+      <CreateProjectBanner />
+    </main>
+  )
+}

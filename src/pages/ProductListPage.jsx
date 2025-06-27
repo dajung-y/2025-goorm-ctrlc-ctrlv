2619944c1abb43ce128d5364596e1167dd517e24
hxont <queenly9@icloud.com>
2025-06-27 16:36:04 +0900

    feat: 패션카테고리

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import ProductCard from "../components/ProductCard.jsx";
+import product from '../data/products.json';
 
 export default function ProductListPage() {
-    const productList = Array.from({ length: 16 });
 
   return (
       <>
           {/* sub category */}
-          <div className="h-[10vh] flex items-center mx-[8%]">
+          <div className="h-[10vh] flex items-center mx-[10%]">
               <button className="bg-black text-white text-center w-[50px] h-[30px] rounded-[8px]">
                   <span className="text-sm font-bold">전체</span>
               </button>
           </div>
 
           {/* product list */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 px-[8%] pb-10">
-              {productList.map((_, idx) => (
-                  <ProductCard key={idx} />
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-[10%] pb-10">
+              {product.map((product, idx) => (
+                  <ProductCard key={idx} product={product}/>
               ))}
           </div>
 

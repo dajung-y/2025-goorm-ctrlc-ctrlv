909c76f0cda3ca61ab45d43960363233f1b24b59
kimhana415 <khnaa415@gmail.com>
2025-07-01 15:15:21 +0900

    feat: 하나: 더 보기 클릭 시 HeaderMore 컴포넌트 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import HeaderGnb from './HeaderGnb'
 
@@ -6,8 +6,7 @@ import HeaderGnb from './HeaderGnb'
 import logo from '../../assets/logo.svg'
 import { HiOutlineMapPin } from 'react-icons/hi2'
 import { SlArrowDown } from 'react-icons/sl'
-import { HiOutlineLightBulb, HiOutlineShare } from 'react-icons/hi'
-import { TbHomeSearch } from 'react-icons/tb'
+import HeaderMore from './HeaderMore'
 
 // 로고
 const Logo = () => (
@@ -20,47 +19,61 @@ const Logo = () => (
 
 const headerNavs = [
   {
+    type: 'link',
     label: '오픈예정',
     href: '/'
   },
   {
+    type: 'link',
     label: '펀딩 +',
     href: '/'
   },
   {
+    type: 'link',
     label: '프리오더',
     href: '/'
   },
   {
+    type: 'link',
     label: '스토어',
     href: '/'
   },
+  {
+    type: 'dropdown',
+    label: '더보기',
+  },
 ]
 
-const Nav = () => (
+const Nav = ({ isMoreOpen, setIsMoreOpen }) => (
   <nav>
     <ul className='flex gap-8 items-center'>
       {
-        headerNavs.map((nav, i) => (
+        headerNavs.map((nav, i) => {
+          switch (nav.type) {
+            case 'link':
+              return (
+                <li key={i} className='text-lg font-semibold'>
+                  <Link to={nav.href}>{nav.label}</Link>
+                </li>
+              )
 
-          <li key={i} className='text-lg font-semibold'>
-            <Link to={nav.href}>{nav.label}</Link>
-          </li>
+            case 'dropdown':
+              return (
+                <li key={i} className='text-lg font-semibold'>
+                  <button
+                    onClick={() => setIsMoreOpen(!isMoreOpen)}
+                    className='flex items-center gap-2 '
+                  >
+                    {nav.label}
+                    <SlArrowDown size="14" className={`transition-transform  ${isMoreOpen ? 'rotate-180' : 'rotate-0'} `} />
+                  </button>
+                </li>
+              )
+          }
 
-        ))
+        })
       }
-      <li className='text-lg font-semibold'>
-        <button className='flex items-center gap-2 '>
-          더보기
-          <SlArrowDown size="14" />
-        </button>
-      </li>
     </ul>
-
-    {/* 더보기 클릭 시 */}
-    <div>
-
-    </div>
   </nav>
 )
 
@@ -92,15 +105,23 @@ const Util = () => (
       utilItems.map((item, i) => {
         switch (item.type) {
           case 'icon':
-            return <button key={i}>{item.icon}</button>;
+            return (
+              <button key={i}>{item.icon}</button>
+            )
+
           case 'link':
-            return <Link key={i} to={item.href} className='text-sm'>{item.label}</Link>
+            return (
+              <Link key={i} to={item.href} className='text-sm'>{item.label}</Link>
+            )
+
           case 'button':
             return (
-              <Link key={i} to={item.href}>
-                <button className='text-sm text-white rounded-sm px-4 h-[40px] bg-cyan-500'>
-                  {item.label}
-                </button>
+              <Link
+                key={i}
+                to={item.href}
+                className='flex items-center text-sm text-white rounded-sm px-4 h-[40px] bg-[var(--color-primary)]'
+              >
+                {item.label}
               </Link>
             )
         }
@@ -110,6 +131,7 @@ const Util = () => (
 )
 
 export default function Header() {
+  const [isMoreOpen, setIsMoreOpen] = useState(false);
   return (
     <>
       <header className='w-full tracking-[-0.5px]'>
@@ -117,12 +139,12 @@ export default function Header() {
           <Logo />
 
           <div className="flex justify-between items-center w-full">
-            <Nav />
+            <Nav isMoreOpen={isMoreOpen} setIsMoreOpen={setIsMoreOpen} />
             <Util />
           </div>
         </div>
       </header>
-
+      {isMoreOpen && <HeaderMore />}
       <HeaderGnb />
     </>
   )

ff3813a4c60c7116f145cfaf61ed0406bae93cc5
dajung-y <0352wjd@naver.com>
2025-07-03 17:26:45 +0900

    fix: 다정: 데이터 변경

--- src/pages/FashionListPage.jsx
+++ src/pages/FashionListPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ProductList from '../components/ProductList';
-import product from '../data/fashionProducts.json';
+// import product from '../data/fashionProducts.json';
+import product from '../data/productDetail.json'
 
+// 데이터 구조분해할당
+const simpleProduct = product.map(item => ({
+
+    id: item.id,
+    title: item.title,
+    price: item.price,
+    rating: item.rating,
+    reviewCount: item.reviewCount,
+    image: item.image,
+}));
+ 
 export default function FashionListPage() {
     console.log("product:", product);
     return (
-        <ProductList products={product} />
+        <ProductList products={simpleProduct} />
     )
 }

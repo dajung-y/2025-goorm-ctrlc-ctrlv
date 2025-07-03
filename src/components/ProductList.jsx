e1b018487fc2b40fb20b25c05ec4df6049d3140d
dajung-y <0352wjd@naver.com>
2025-07-03 17:26:18 +0900

    fix: 다정: index 수정

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -13,8 +13,8 @@ export default function ProductList({ products }) {
 
             {/* product list */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-[5%] pb-15">
-                {products.map((product, idx) => (
-                    <ProductCard key={idx} product={product} />
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} /> // key값 변경
                 ))}
             </div>
         </section>
